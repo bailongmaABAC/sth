09ea19e0f46096d2b2e3315a670dee10b7b80df9
bailongmaABAC <134177650+bailongmaABAC@users.noreply.github.com>
2024-12-01 12:27:18 +0800

    Create install_debian.sh

---
+++ install_debian.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "建议授予内部存储访问权限"
+termux-setup-storage
+
+pkg install wget proot proot-distro -y
+sed -i 's/github.com/ghp.ci\/github.com/' $PREFIX/etc/proot-distro/debian.sh
+proot-distro install debian
+
+echo "#!/data/data/com.termux/files/usr/bin/bash
+proot-distro login debian --shard-tmp
+" > $HOME/debian.sh
+chmod +x $HOME/debian.sh
+echo "Debian已安装完成，正在进入系统"
+
+$HOME/debian.sh
