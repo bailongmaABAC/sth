a00562188223681d6755c4856aa07b9c6a76c9c1
bailongmaABAC <134177650+bailongmaABAC@users.noreply.github.com>
2024-12-01 12:49:10 +0800

    Update install_debian.sh

--- install_debian.sh
+++ install_debian.sh
@@ -7,7 +7,7 @@ sed -i 's/github.com/ghp.ci\/github.com/' $PREFIX/etc/proot-distro/debian.sh
 proot-distro install debian
 
 echo "#!/data/data/com.termux/files/usr/bin/bash
-proot-distro login debian --shard-tmp
+proot-distro login debian --shared-tmp
 " > $HOME/debian.sh
 chmod +x $HOME/debian.sh
 echo "Debian已安装完成，正在进入系统"
